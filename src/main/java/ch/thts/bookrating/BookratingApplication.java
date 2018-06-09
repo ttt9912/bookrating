9e9f1b1bf75b8be091c905b7f7004dcad2fc5532
ttt9912 <q123456>
2018-06-09 08:38:43 +0200

    initial

---
+++ src/main/java/ch/thts/bookrating/BookratingApplication.java
@@ -0,0 +1,12 @@
+package ch.thts.bookrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookratingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookratingApplication.class, args);
+    }
+}
---
+++ src/test/java/ch/thts/bookrating/BookratingApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.thts.bookrating;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookratingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
